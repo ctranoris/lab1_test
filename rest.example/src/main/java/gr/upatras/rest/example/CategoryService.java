64f2b0ddeff3201c9f1957269b1fbd068c6fc707
Christos Tranoris <tranoris@gmail.com>
2022-04-29 00:47:42 +0300

    first changes

---
+++ rest.example/src/main/java/gr/upatras/rest/example/CategoryService.java
@@ -0,0 +1,85 @@
+package gr.upatras.rest.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author ctranoris
+ *
+ */
+
+@Service
+public class CategoryService implements ICategoryService {
+
+	// creating an object of ArrayList
+	List<Category> categories = new ArrayList<Category>();
+
+	@Autowired
+	private IProductService productService;
+
+	int ix = 10;
+	
+	
+	public CategoryService() {
+		super();
+
+//		categories.add( new Category(1, "TV"));
+//		categories.add( new Category(2, "Electronics"));
+//		categories.add( new Category(3, "Home & Kitchen"));
+	}
+
+	@Override
+	public List<Category> findAll() {
+		return categories;
+	}
+
+	@Override
+	public Category findById(int id) {
+		for (Category c : categories) {
+			if (c.getId() == id) {
+				return c;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public Category addCategory(Category catToAdd) {
+		ix = ix +1; //increase product index
+		Category c = new Category(ix, catToAdd.getName());
+		for (Product p : catToAdd.getProducts()) {
+			Product productToAdd = productService.findById(p.getId());
+			c.getProducts().add(productToAdd);
+		}
+		categories.add( c );
+		return c;
+	}
+
+	@Override
+	public Category editCategory(Category c) {
+		Category editCat = findById( c.getId() );
+		if ( editCat != null ) {
+			editCat.setName( c.getName() );
+			editCat.setProducts( c.getProducts()  );
+			
+			return editCat;
+		}
+		return null;
+	}
+
+	@Override
+	public Void deleteCategory(int id) {
+		for (Category p : categories) {
+			if (p.getId() == id) {
+				categories.remove(p);
+				break;
+			}
+		}
+		return null;
+
+	}
+
+}
