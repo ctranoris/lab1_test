1b94c119b68d556ce48de3c9e7aec73eeaaad443
Christos Tranoris <tranoris@gmail.com>
2022-05-23 11:27:53 +0300

    added comments to all cmethods

--- rest.example/src/main/java/gr/upatras/rest/example/Product.java
+++ rest.example/src/main/java/gr/upatras/rest/example/Product.java
@@ -21,46 +21,83 @@ public class Product {
 	/** 	 */
 	private int noofproduct;
 
+	/**
+	 * @return
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * @param id
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 
+	/**
+	 * @return
+	 */
 	public String getPname() {
 		return pname;
 	}
 
+	/**
+	 * @param pname
+	 */
 	public void setPname(String pname) {
 		this.pname = pname;
 	}
 
+	/**
+	 * @return
+	 */
 	public String getBatchno() {
 		return batchno;
 	}
 
+	/**
+	 * @param batchno
+	 */
 	public void setBatchno(String batchno) {
 		this.batchno = batchno;
 	}
 
+	/**
+	 * @return
+	 */
 	public double getPrice() {
 		return price;
 	}
 
+	/**
+	 * @param price
+	 */
 	public void setPrice(double price) {
 		this.price = price;
 	}
 
+	/**
+	 * @return
+	 */
 	public int getNoofproduct() {
 		return noofproduct;
 	}
 
+	/**
+	 * @param noofproduct
+	 */
 	public void setNoofproduct(int noofproduct) {
 		this.noofproduct = noofproduct;
 	}
 
+	/**
+	 * @param id
+	 * @param pname
+	 * @param batchno
+	 * @param price
+	 * @param noofproduct
+	 */
 	public Product(int id, String pname, String batchno, double price, int noofproduct) {
 		super();
 		this.id = id;
